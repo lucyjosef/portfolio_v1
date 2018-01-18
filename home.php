c65c84a51ec6de0eb34c5ed7c94f7ebc381fa7f8
lucyjosef <luciejoseph@ymail.com>
2018-01-18 20:19:15 +0100

    first commit

---
+++ home.php
@@ -0,0 +1,12 @@
+<!-- HOME -->
+
+<section id="top">
+    <div class="container flexbox">
+        <img src="ressources/img/main.jpg" class="img-circle">
+        <div>
+            <h1>Hello !</h1>
+            <p>Je suis Lucie , Lyonnaise depuis 2009. Infographiste repentie, en reconversion dans le développement web en 2016. Aujourd'hui en formation avec l'IT-Akademy. Dans le cadres de cette de cette formation, je recherche une opportunité d'alternance à partir du mois de mars.</p>
+            <a href="#contact-items" class="btn btn-danger">CONTACT !</a>
+        </div>
+    </div>
+</section>
