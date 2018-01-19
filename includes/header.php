82656da0640e4dc2c866e864f0699eb1259cdd97
lucyjosef <luciejoseph@ymail.com>
2018-01-19 18:20:13 +0100

    update

---
+++ includes/header.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <title>Lucie JOSEPH | Développeur</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <!-- Font awesome -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <!-- Font and perso imports -->
+        <link type="text/css" rel="stylesheet" href="../ressources/style.css">
+        <link href="https://fonts.googleapis.com/css?family=Noto+Serif|PT+Sans+Narrow" rel="stylesheet">
+        <!-- <link rel="stylesheet" type="text/css" href="style.css"> -->
+        <!-- My scripts -->
+        <script src="./ressources/scriptFormiport.js"></script>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <nav>
+                <ul class="ul-no-style flexbox">
+                    <li><a href="#skills">SKILLS</a></li>
+                    <li><a href="#profile">PROFILE</a></li>
+                    <li><a href="#real">REALISATIONS</a></li>
+                    <li><a href="#form-section">CONTACT</a></li>
+                </ul>
+            </nav>
---
+++ includes/skills.php
@@ -0,0 +1,79 @@
+<section id="skills">
+    <div class="container">
+
+        <!-- ARTICLES -->
+
+        <article class=" col-sm-6">
+            <div class="flexbox">
+                <div class="border-right">
+                    <span class="icon-strong"><i class="fa fa-terminal" aria-hidden="true"></i></span>
+                </div>
+                <div class="reduce-width">
+                    <h3>Front</h3>
+                    <p class="muted">Lorem ipsum dolor sit amet, eam iisque molestie ocurreret ex, vim augue doctus convenire ei. At ludus efficiantur sed. Eam te eirmod complectitur. Te harum civibus pri. Graeci salutandi prodesset te sit, id has case minimum, nostro suscipiantur eu cum. </p>
+                    <ul class="tag-list ul-no-style flexbox wrap">
+                        <li><i class="fa fa-tag" aria-hidden="true"></i> html / css</li>
+                        <li><i class="fa fa-tag" aria-hidden="true"></i> UX design</li>
+                        <li><i class="fa fa-tag" aria-hidden="true"></i> less</li>
+                        <li><i class="fa fa-tag" aria-hidden="true"></i> Jquery</li>
+                        <li><i class="fa fa-tag" aria-hidden="true"></i> webdesign</li>
+                    </ul>
+                </div>
+            </div>
+        </article>
+        <article class=" col-sm-6">
+            <div class="flexbox">
+                <div class="border-right">
+                    <span class="icon-strong"><i class="fa fa-terminal" aria-hidden="true"></i></span>
+                </div>
+                <div class="reduce-width">
+                    <h3>Front</h3>
+                    <p class="muted">Lorem ipsum dolor sit amet, eam iisque molestie ocurreret ex, vim augue doctus convenire ei. At ludus efficiantur sed. Eam te eirmod complectitur. Te harum civibus pri. Graeci salutandi prodesset te sit, id has case minimum, nostro suscipiantur eu cum. </p>
+                    <ul class="tag-list ul-no-style flexbox wrap">
+                        <li><i class="fa fa-tag" aria-hidden="true"></i> html / css</li>
+                        <li><i class="fa fa-tag" aria-hidden="true"></i> UX design</li>
+                        <li><i class="fa fa-tag" aria-hidden="true"></i> less</li>
+                        <li><i class="fa fa-tag" aria-hidden="true"></i> Jquery</li>
+                        <li><i class="fa fa-tag" aria-hidden="true"></i> webdesign</li>
+                    </ul>
+                </div>
+            </div>
+        </article>
+        <article class=" col-sm-6">
+            <div class="flexbox">
+                <div class="border-right">
+                    <span class="icon-strong"><i class="fa fa-terminal" aria-hidden="true"></i></span>
+                </div>
+                <div class="reduce-width">
+                    <h3>Front</h3>
+                    <p class="muted">Lorem ipsum dolor sit amet, eam iisque molestie ocurreret ex, vim augue doctus convenire ei. At ludus efficiantur sed. Eam te eirmod complectitur. Te harum civibus pri. Graeci salutandi. </p>
+                    <ul class="tag-list ul-no-style flexbox wrap">
+                        <li><i class="fa fa-tag" aria-hidden="true"></i> html / css</li>
+                        <li><i class="fa fa-tag" aria-hidden="true"></i> UX design</li>
+                        <li><i class="fa fa-tag" aria-hidden="true"></i> less</li>
+                        <li><i class="fa fa-tag" aria-hidden="true"></i> Jquery</li>
+                        <li><i class="fa fa-tag" aria-hidden="true"></i> webdesign</li>
+                    </ul>
+                </div>
+            </div>
+        </article>
+        <article class="col-sm-6">
+            <div class="flexbox">
+                <div class="border-right">
+                    <span class="icon-strong"><i class="fa fa-terminal" aria-hidden="true"></i></span>
+                </div>
+                <div class="reduce-width">
+                    <h3>Front</h3>
+                    <p class="muted">Lorem ipsum dolor sit amet, eam iisque molestie ocurreret ex, vim augue doctus convenire ei. At ludus efficiantur sed. Eam te eirmod complectitur. Te harum civibus pri. Graeci salutandi prodesset te sit, id has case minimum, nostro suscipiantur eu cum. Id has case minimum, nostro suscipiantur eu cum. </p>
+                    <ul class="tag-list ul-no-style flexbox wrap">
+                        <li><i class="fa fa-tag" aria-hidden="true"></i> html / css</li>
+                        <li><i class="fa fa-tag" aria-hidden="true"></i> UX design</li>
+                        <li><i class="fa fa-tag" aria-hidden="true"></i> less</li>
+                        <li><i class="fa fa-tag" aria-hidden="true"></i> Jquery</li>
+                        <li><i class="fa fa-tag" aria-hidden="true"></i> webdesign</li>
+                    </ul>
+                </div>
+            </div>
+        </article>
+    </div>
+</section>
